ec939c1139d1c8385d62e8a710c1b0e61f36ebb0
tokyandria001 <>
2026-01-15 10:08:14 +0100

    delete front

--- backend/src/Controller/RegisterController.php
+++ backend/src/Controller/RegisterController.php
@@ -1,14 +1,13 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Api;
 
 use App\Entity\Inscription;
 use App\Repository\InscriptionRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class RegisterController extends AbstractController
@@ -22,30 +21,23 @@ final class RegisterController extends AbstractController
 
         $data = json_decode($request->getContent(), true);
 
-        // Vérification : tous les champs obligatoires existent
         $required = ['firstname', 'lastname', 'mail', 'password'];
         foreach ($required as $field) {
-            if (!isset($data[$field])) {
+            if (empty($data[$field])) {
                 return new JsonResponse(["error" => "Missing field: $field"], 400);
             }
         }
 
-        // Vérification : email déjà utilisé ?
         if ($repo->findOneBy(['mail' => $data['mail']])) {
             return new JsonResponse(["error" => "Email déjà utilisé"], 409);
         }
 
-        // Création du user
         $user = new Inscription();
         $user->setFirstname($data['firstname']);
         $user->setLastname($data['lastname']);
         $user->setMail($data['mail']);
+        $user->setPassword(password_hash($data['password'], PASSWORD_BCRYPT));
 
-        // Hash mot de passe
-        $hashedPassword = password_hash($data['password'], PASSWORD_BCRYPT);
-        $user->setPassword($hashedPassword);
-
-        // Champs optionnels
         $user->setPhone($data['phone'] ?? null);
         $user->setBudget($data['budget'] ?? []);
         $user->setActivity($data['activity'] ?? []);
@@ -55,8 +47,8 @@ final class RegisterController extends AbstractController
         $em->flush();
 
         return new JsonResponse([
-            "message" => "Inscription réussie",
-            "userId" => $user->getId()
+            'message' => 'Inscription réussie',
+            'userId' => $user->getId()
         ], 201);
     }
 }
